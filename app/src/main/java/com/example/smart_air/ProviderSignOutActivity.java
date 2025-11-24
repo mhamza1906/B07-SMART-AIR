19cb6aca06bc8da20bc653901ae9502f32746dc9
Kei <oddfriend11@gmail.com>
2025-11-23 21:39:44 -0500

    Updated AndroidManifest.xml

--- app/src/main/java/com/example/smart_air/ProviderSignOutActivity.java
+++ app/src/main/java/com/example/smart_air/ProviderSignOutActivity.java
@@ -1,4 +1,17 @@
 package com.example.smart_air;
 
-public class ProviderSignOutActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProviderSignOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dummy_page);
+    }
 }
