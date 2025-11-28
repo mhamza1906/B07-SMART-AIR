af21f9d03dd7a049f890b7dee2a0c9b09f064690
Kei <oddfriend11@gmail.com>
2025-11-27 22:53:06 -0500

    Now Parent Schedule dropdown queries from database

--- app/src/main/java/com/example/smart_air/ParentScheduleActivity.java
+++ app/src/main/java/com/example/smart_air/ParentScheduleActivity.java
@@ -3,15 +3,60 @@ package com.example.smart_air;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParentScheduleActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dummy_page);
+        setContentView(R.layout.activity_parent_schedule);
+
+        String parentID = getIntent().getStringExtra("parentID");
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        firestore = FirebaseFirestore.getInstance();
+        Spinner childSpinner = (Spinner) findViewById(R.id.childspinner);
+        CollectionReference childCollection = firestore.collection("parent-child").document(parentID).collection("child");
+
+        List<String> children = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.activity_parent_schedule,children);
+        adapter.setDropDownViewResource(R.layout.activity_parent_schedule);
+        childSpinner.setAdapter(adapter);
+
+        childCollection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String child = document.getString("fName")+" "+document.getString("lName");
+                        children.add(child);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
     }
 }
