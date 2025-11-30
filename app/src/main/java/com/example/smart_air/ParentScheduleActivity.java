297215d18f454953fc5d9b8e050fa9413aecf14a
Kei <oddfriend11@gmail.com>
2025-11-29 22:26:31 -0500

    Refined date picker, added text view for selected date, started set event functionality

--- app/src/main/java/com/example/smart_air/ParentScheduleActivity.java
+++ app/src/main/java/com/example/smart_air/ParentScheduleActivity.java
@@ -1,11 +1,14 @@
 package com.example.smart_air;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,22 +23,26 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class ParentScheduleActivity extends AppCompatActivity {
+public class ParentScheduleActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    private DatabaseReference mDatabase;
     private FirebaseFirestore firestore;
-
+    TextView chosenDate;
+    Calendar calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_schedule);
+        calendar = Calendar.getInstance();
 
         String parentID = getIntent().getStringExtra("parentID");
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
         firestore = FirebaseFirestore.getInstance();
         Spinner childSpinner = (Spinner) findViewById(R.id.childspinner);
         CollectionReference childCollection = firestore.collection("parent-child").document(parentID).collection("child");
@@ -58,5 +65,39 @@ public class ParentScheduleActivity extends AppCompatActivity {
             }
         });
 
+        chosenDate = (TextView)findViewById(R.id.ChosenDate);
+        Button pickDateButton = (Button)findViewById(R.id.pickDate);
+        pickDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(),"datePicker");
+            }
+        });
+
+        Button setDateButton = (Button)findViewById(R.id.setDate);
+        setDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create document in "planned-schedule" collection, according to "childID" document with put()
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH)+1;
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                Map<String, Object> calData = new HashMap<>();
+                // calData.put( data here );
+                calData.put("date",Integer.toString(year)+"-"+Integer.toString(month)+"-"+Integer.toString(day));
+                calData.put("taken",false);
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        calendar.set(Calendar.YEAR,year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,day);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
+        chosenDate.setText(selectedDate);
     }
 }
