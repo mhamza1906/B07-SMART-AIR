4c7e89246764bf86bfcd094da9cb10773bee1fdf
Kei <oddfriend11@gmail.com>
2025-11-30 17:48:38 -0500

    Finished first version of parent planned schedule page, added functionality for setting events and number of controllers

--- app/src/main/java/com/example/smart_air/ParentScheduleActivity.java
+++ app/src/main/java/com/example/smart_air/ParentScheduleActivity.java
@@ -4,11 +4,13 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,33 +32,42 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ParentScheduleActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class ParentScheduleActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
 
     private FirebaseFirestore firestore;
     TextView chosenDate;
     Calendar calendar;
+    Spinner childSpinner;
+    String selectedChildID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_schedule);
-        calendar = Calendar.getInstance();
 
+        calendar = Calendar.getInstance();
         String parentID = getIntent().getStringExtra("parentID");
 
         firestore = FirebaseFirestore.getInstance();
-        Spinner childSpinner = (Spinner) findViewById(R.id.childspinner);
+        childSpinner = (Spinner) findViewById(R.id.childspinner);
         CollectionReference childCollection = firestore.collection("parent-child").document(parentID).collection("child");
 
         List<String> children = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.activity_parent_schedule,children);
         adapter.setDropDownViewResource(R.layout.activity_parent_schedule);
         childSpinner.setAdapter(adapter);
+        childSpinner.setOnItemSelectedListener(this);
 
         childCollection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()) {
+                    int i = 0;
                     for (QueryDocumentSnapshot document : task.getResult()) {
+                        if(i==0) {
+                            selectedChildID = document.getString("childID");
+                        }
+                        i += 1;
                         String child = document.getString("fName")+" "+document.getString("lName");
                         children.add(child);
                     }
@@ -75,6 +86,32 @@ public class ParentScheduleActivity extends AppCompatActivity implements DatePic
             }
         });
 
+        TextView controllerNumber = (TextView)findViewById(R.id.controllerNumberText);
+        Button increment = (Button)findViewById(R.id.incrementButton);
+        increment.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+                                             int counter = Integer.parseInt(controllerNumber.getText().toString());
+                                             counter += 1;
+                                             controllerNumber.setText(String.valueOf(counter));
+                                         }
+                                     });
+
+        Button decrement = (Button) findViewById(R.id.decrementButton);
+        decrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int counter = Integer.parseInt(controllerNumber.getText().toString());
+                if(counter > 1) {
+                    counter -= 1;
+                } else {
+                    Toast controllerToast = Toast.makeText(getApplicationContext(),"Cannot set controller number to less than 1",Toast.LENGTH_LONG);
+                    controllerToast.show();
+                }
+                controllerNumber.setText(String.valueOf(counter));
+            }
+        });
+
         Button setDateButton = (Button)findViewById(R.id.setDate);
         setDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,8 +123,13 @@ public class ParentScheduleActivity extends AppCompatActivity implements DatePic
 
                 Map<String, Object> calData = new HashMap<>();
                 // calData.put( data here );
-                calData.put("date",Integer.toString(year)+"-"+Integer.toString(month)+"-"+Integer.toString(day));
+                String controllerNum = controllerNumber.getText().toString();
                 calData.put("taken",false);
+                calData.put("controllerNumber",Integer.parseInt(controllerNum));
+
+                // Create document/overwrite existing document for this date
+                firestore.collection("planned-schedule").document(selectedChildID).collection("Schedules")
+                        .document(Integer.toString(year)+"-"+Integer.toString(month)+"-"+Integer.toString(day)).set(calData);
             }
         });
     }
@@ -99,5 +141,16 @@ public class ParentScheduleActivity extends AppCompatActivity implements DatePic
         calendar.set(Calendar.DAY_OF_MONTH,day);
         String selectedDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
         chosenDate.setText(selectedDate);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Map<String, Object> child = (Map<String, Object>) parent.getItemAtPosition(position);
+        selectedChildID = (String) child.get("childID");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
